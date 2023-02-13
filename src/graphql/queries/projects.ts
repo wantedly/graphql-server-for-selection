eb676b4c890f9ec9b2e25ecc23bde52e30099612
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-02-13 23:45:36 +0900

    style(projects): Rename variable

--- src/graphql/queries/projects.ts
+++ src/graphql/queries/projects.ts
@@ -3,9 +3,9 @@ import { list, queryField } from "nexus";
 export const projects = queryField("projects", {
   type: list("Project"),
   resolve: async (_root, _args, { prismaClient }) => {
-    const p = await prismaClient.project.findMany({
+    const projects = await prismaClient.project.findMany({
       include: { staffings: { include: { user: true } } },
     });
-    return p;
+    return projects;
   },
 });
